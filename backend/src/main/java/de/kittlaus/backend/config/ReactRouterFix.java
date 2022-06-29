1745221dc8bf11c6693ea31a4086540ad508da0b
Droggelbecher92 <thomaskittlaus@gmail.com>
2022-06-29 20:08:46 +0200

    update properties

--- backend/src/main/java/de/kittlaus/backend/config/ReactRouterFix.java
+++ backend/src/main/java/de/kittlaus/backend/config/ReactRouterFix.java
@@ -1,7 +1,8 @@
 package de.kittlaus.backend.config;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+@Controller
 public class ReactRouterFix {
     @RequestMapping(value = "/**/{[path:[^\\.]*}")
     public String forwardToRouteUrl() {
